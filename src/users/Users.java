5e0d09c793736f15388c0db9c6668b9b58e91be3
Aniket <locked.ani@gmail.com>
2016-07-15 02:11:08 +0530

    Modifying .war file

--- src/users/Users.java
+++ src/users/Users.java
@@ -11,32 +11,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Users
- */
+
 @WebServlet("/second")
 public class Users extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Users() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String s = null;
 
         try {
             
-	    // run the Unix "ps -ef" command
             // using the Runtime exec method:
-            Process p = Runtime.getRuntime().exec("ps");
+            Process p = Runtime.getRuntime().exec("w");
             
             BufferedReader stdInput = new BufferedReader(new 
                  InputStreamReader(p.getInputStream()));
@@ -51,31 +37,19 @@ public class Users extends HttpServlet {
     		out.print("<title>Users</title>");
     		out.print("</head>");
     		out.print("<body>");
-    		//out.print("Hello World !");
-    		out.print("Here is the standard output of the command:\n");
+    		out.print("Information of logged in Users:\n");
     		while ((s = stdInput.readLine()) != null) {
-                out.println(s);
-            }
-    		out.print("<br><br>");
-    		out.print("Here is the standard error of the command (if any):\n");
-    		while ((s = stdError.readLine()) != null) {
-                out.print(s);
+                out.print(s + " <br>");
             }
     		out.print("</body>");
     		out.print("</html>");
         }
         catch (IOException e) {
-            //out.println("exception happened - here's what I know: ");
             e.printStackTrace();
-            //System.exit(-1);
         }
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
-
 }
